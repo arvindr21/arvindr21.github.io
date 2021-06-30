9daf037cdc7587787fde46dce91a0f0aa513711d
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2021-06-30 17:05:23 +0530

    update blog name

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header>
       <div className='container'>
         <Link href='/' passHref>
-          <h2>Dev Blog</h2>
+          <h2>Arvind Blogs</h2>
         </Link>
       </div>
     </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <title>Dev Blog</title>
+        <title>Arvind Blogs</title>
       </Head>
 
       <div className='posts'>
